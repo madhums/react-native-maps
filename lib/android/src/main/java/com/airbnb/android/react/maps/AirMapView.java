49ef44af26bd22ec2f9ba93176a4c26891a8191c
Christoph Lambio <c.lambio@datalyze-solutions.com>
2018-07-19 14:33:31 +0200

    Added import android.location.Location; to AirMapView.java. Removing it breaked compiling it

--- lib/android/src/main/java/com/airbnb/android/react/maps/AirMapView.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/AirMapView.java
@@ -19,6 +19,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.location.Location;
 
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
